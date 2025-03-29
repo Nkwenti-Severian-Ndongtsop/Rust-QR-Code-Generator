486e4edf111d9f65181c68fdd89f443713e2f29b
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-29 08:28:25 +0100

    chore(file): user input

---
+++ server/src/main.rs
@@ -0,0 +1,33 @@
+
+
+
+mod routes;
+mod qr_generator;
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:7878";
+    let listener = tokio::net::TcpListener::bind(addr).await;
+    let listener = match listener {
+        Ok(listener) => {
+            println!("The server is running on: http://{}", addr);
+            listener
+        }
+        Err(e) => {
+            eprint!("Couldn't bind addres: {}", e);
+            return;
+        }
+    };
+
+    
+    let response = axum::serve(listener, routes::create_routes()).await;
+
+    match response {
+        Ok(_) => {
+            println!("Successful response from Server")
+        }
+        Err(e) => {
+            eprintln!("Error response from server: {}", e)
+        }
+    }
+}
