4e8b800a5b00675b07f4ee88f616970df2e37bf8
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-29 08:28:51 +0100

    chore(file): impmented routing to handle qr-code genrator

---
+++ server/src/routes.rs
@@ -0,0 +1,38 @@
+use axum::{
+    response::IntoResponse, routing::{get, post}, Json, Router
+};
+use serde::Deserialize;
+
+use crate::qr_generator::generate_qr;
+
+
+#[derive(Deserialize)]
+pub struct QRRequest {
+    data: String,
+    size: Option<u32>,
+    format: String,
+}
+
+pub async fn generate_qr_handler(Json(payload): Json<QRRequest>) -> impl IntoResponse {
+
+    
+    let size = payload.size.unwrap_or(300);
+    let format = payload.format;
+    let qr_image = generate_qr(payload.data.as_str(), size, format.as_str());
+
+    println!("Thanks for using QR-Code: by @Nkwenti@severian\n");
+
+    qr_image
+}
+
+
+
+pub async fn root() -> &'static str {
+    "Hello Qr_code Running"
+}
+
+pub fn create_routes() -> Router {
+        Router::new()
+            .route("/generate-qr", post(generate_qr_handler))
+            .route("/", get(root))
+    }
