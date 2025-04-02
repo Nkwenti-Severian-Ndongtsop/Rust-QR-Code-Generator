22868b1a89d435fe6fa0419fdd1d0daae7b1a921
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-04-02 14:03:37 +0100

    chore(file): added cli

--- Qr_code/src/main.rs
+++ Qr_code/src/main.rs
@@ -1,3 +1,4 @@
+use db::connect;
 use reqwest::Client;
 use serde::Deserialize;
 use serde::Serialize;
@@ -9,7 +10,7 @@ mod db;
 
 #[derive(Serialize, Deserialize)]
 pub struct QRRequest {
-    data: String,  
+    data: String,
     size: Option<u32>,
     format: String,
 }
@@ -23,7 +24,6 @@ async fn main() {
         eprintln!("Usage: cargo run <data> <format> <size>");
         return;
     }
-
 
     let request_input = QRRequest {
         data: args[1].clone(),
@@ -43,7 +43,7 @@ async fn main() {
                     }
                 };
 
-                let output_dir = "QR_CODES";
+                let output_dir = "~/QR_CODES";
                 std::fs::create_dir_all(output_dir).unwrap_or_else(|e| {
                     eprintln!("Error creating output directory: {}", e);
                     std::process::exit(1);
@@ -59,17 +59,31 @@ async fn main() {
                 };
 
                 if let Err(e) = file.write_all(&body) {
-                    eprintln!("Error writing to file: {}", e);
+                    return eprintln!("Error writing to file: {}", e);
                 } else {
+                    let pool = connect().await;
+
+                    match sqlx::query(
+                        "INSERT INTO DATA (data_input, image_type, image_size) VALUES ($1, $2, $3)",
+                    )
+                    .bind(request_input.data)
+                    .bind(request_input.format)
+                    .bind(request_input.size.unwrap() as i64)
+                    .execute(&pool)
+                    .await {
+                        Ok(_) => {},
+                        Err(e) => return eprintln!("Error: {}", e),
+                    }
+
                     println!("QR Code saved successfully as {}", file_name);
                     println!("Thanks for using QR-Code: by @Nkwenti@Severian\n");
                 }
             } else {
-                eprintln!("Failed to generate QR Code. Status: {}", response.status());
+                return eprintln!("Failed to generate QR Code. Status: {}", response.status());
             }
         }
         Err(e) => {
-            eprintln!("Error occurred while sending request: {}", e);
+            return eprintln!("Error occurred while sending request: {}", e);
         }
     }
 }
