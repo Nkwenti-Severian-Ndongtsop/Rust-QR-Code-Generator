9faecb1764b263168c10ed7e61760cce0642c1aa
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-04-03 09:35:53 +0100

    chore(file): fix request response

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ async fn main() {
                 };
                 
 
-            match sqlx::query("INSERT INTO DATA (data_input, image_type, image_size) VALUES ($1, $2, $3)")
+            match sqlx::query("INSERT INTO data (data_input, image_type, image_size) VALUES ($1, $2, $3)")
                 .bind(&request_input.data)
                 .bind(&request_input.format)
                 .bind(request_input.size as i64)
